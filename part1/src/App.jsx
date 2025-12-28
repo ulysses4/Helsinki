3a86ce331a7f342a6b0371fd15bd2e8424295741
ICashyn <igor.cashyn@gmail.com>
2025-12-28 14:32:27 -0500

    'update'

---
+++ part1/src/App.jsx
@@ -0,0 +1,20 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const friends = ['peter', 'sam']
+
+  return (
+    <div>
+      <p>{friends}</p>
+    </div>
+  )
+}
+
+export default App
